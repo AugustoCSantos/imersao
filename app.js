15a61dbccb99f030da3bad8886611155c9875b2a
Augusto Santos <augustocsantos.dev@gmail.com>
2024-09-08 17:18:22 -0300

    aula 5

--- app.js
+++ app.js
@@ -2,18 +2,41 @@ function pesquisar(){
     let section = document.getElementById("resultados-pesquisa"); //procura no documento HTML por um elemento que tenha o ID "resultados-pesquisa"
     console.log(section);
 
-    let resultados = ""
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+    console.log(campoPesquisa);
+
+    if(!campoPesquisa){
+        section.innerHTML = "<p>Nada foi encontrado. Precisa digitar algo.</p>";
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     for (let dado of dados){
-        resultados += `
-            <div class="item-resultado">
-                <h2>
-                    <a href="#" target="_blank">${dado.titulo}</a>
-                </h2>
-                <p class="descricao-meta">${dado.descricao}</p>
-                <a href="${dado.link}" target="_blank">Mais informações</a>
-            </div>
-        `
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+
+        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            resultados += `
+                <div class="item-resultado">
+                    <h2>
+                        <a href="#" target="_blank">${dado.titulo}</a>
+                    </h2>
+                    <p class="descricao-meta">${dado.descricao}</p>
+                    <a href="${dado.link}" target="_blank">Mais informações</a>
+                </div>
+            `;
+        }
+    }
+
+    if(!resultados){
+        resultados = "<p>Nada foi encontrado.</p>";
     }
 
     section.innerHTML = resultados
--- dados.js
+++ dados.js
@@ -2,54 +2,63 @@ let dados = [ //9 elementos
     {
         titulo: "Valorant",
         descricao: "Valorant é um jogo de tiro em primeira pessoa (FPS) multijogador gratuito, desenvolvido pela Riot Games. Conhecido por seu gameplay competitivo e tático, Valorant combina a precisão dos tradicionais FPS com um sistema de agentes,cada um com habilidades únicas que influenciam o desenrolar das partidas",
-        link: "https://pt.wikipedia.org/wiki/Valorant"
+        link: "https://pt.wikipedia.org/wiki/Valorant",
+        tags: "fps riot multijogador"
     },
 
     {
         titulo: "Elden Ring",
         descricao: "Elden Ring é um RPG de ação desenvolvido pela FromSoftware. Mergulhe em um mundo de fantasia sombrio e explore castelos gigantescos, masmorras tortuosas e paisagens desoladas.",
-        link: "https://pt.wikipedia.org/wiki/Elden_Ring"
+        link: "https://pt.wikipedia.org/wiki/Elden_Ring",
+        tags: "boss souls soulslike"
     },
 
     {
         titulo: "Final Fantasy VII",
         descricao: "Final Fantasy VII é um RPG clássico da Square Enix, conhecido por sua história envolvente e personagens memoráveis. A versão remake foi lançada em 2020, trazendo gráficos atualizados e novas mecânicas de jogo.",
-        link: "https://pt.wikipedia.org/wiki/Final_Fantasy_VII"
+        link: "https://pt.wikipedia.org/wiki/Final_Fantasy_VII",
+        tags: "rpg cloud tifa"
     },
 
     {
         titulo: "God of War",
         descricao: "Uma reinvenção da franquia God of War, com Kratos em um novo cenário nórdico, explorando temas de paternidade e vingança.",
-        link: "https://pt.wikipedia.org/wiki/God_of_War_(2018)"
+        link: "https://pt.wikipedia.org/wiki/God_of_War_(2018)",
+        tags: "kratos atreus deuses"
     },
 
     {
         titulo: "Horizon Zero Dawn",
         descricao: "Um RPG de mundo aberto ambientado em um futuro pós-apocalíptico onde máquinas dominam a Terra. A protagonista, Aloy, é uma caçadora habilidosa em busca de suas origens.",
-        link: "https://pt.wikipedia.org/wiki/Horizon_Zero_Dawn"
+        link: "https://pt.wikipedia.org/wiki/Horizon_Zero_Dawn",
+        tags: "robõs robos aloy"
     },
 
     {
         titulo: "Fortnite",
         descricao: "Um jogo de tiro em terceira pessoa com elementos de construção e um modo battle royale popular. Constantemente atualizado com novas temporadas e eventos.",
-        link: "https://pt.wikipedia.org/wiki/Fortnite"
+        link: "https://pt.wikipedia.org/wiki/Fortnite",
+        tags: "battle-royale battle royale"
     },
 
     {
         titulo: "Celeste",
         descricao: "Um jogo de plataforma 2D com foco em mecânicas desafiadoras e uma história emocionante.",
-        link: "https://pt.wikipedia.org/wiki/Celeste_(jogo_video)"
+        link: "https://pt.wikipedia.org/wiki/Celeste_(jogo_video)",
+        tags: "plataforma 2d "
     },
 
     {
         titulo: "The Witcher 3: Wild Hunt",
         descricao: "Um RPG de mundo aberto ambientado em um universo de fantasia medieval, com uma história rica e personagens complexos.",
-        link: "https://pt.wikipedia.org/wiki/The_Witcher_3:_Wild_Hunt"
+        link: "https://pt.wikipedia.org/wiki/The_Witcher_3:_Wild_Hunt",
+        tags: "geralt Geralt ciri Ciri Yennefer yennefer bruxo"
     },
 
     {
         titulo: "Hollow Knight",
         descricao: "Um metroidvania 2D com gráficos em pixel art, explorando um mundo subterrâneo cheio de criaturas e segredos.",
-        link: "https://pt.wikipedia.org/wiki/Hollow_Knight"
+        link: "https://pt.wikipedia.org/wiki/Hollow_Knight",
+        tags: "silksong metroidvania"
     }
 ]; //Lista com objetos
